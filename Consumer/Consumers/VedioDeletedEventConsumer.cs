5d2a2ffd249d949b9860136a5962a840d2bfa55d
unknown <cogdotnet08@iiht.tech>
2022-05-21 00:02:51 +0530

    save

---
+++ Consumer/Consumers/VedioDeletedEventConsumer.cs
@@ -0,0 +1,18 @@
+﻿using Consumer.Events;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consumer.Consumers
+{
+    public class VedioDeletedEventConsumer:IConsumer<VedioDeletedEvents>
+    {
+        public Task Consume(ConsumeContext<VedioDeletedEvents> context)
+        {
+            var message = context.Message.Title;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Producer/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit.KafkaIntegration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Producer.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private ITopicProducer<VedioDeletedEvents> _topicProducer;
+        public HomeController(ITopicProducer<VedioDeletedEvents> topicProducer)
+        {
+            _topicProducer = topicProducer;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]string title)
+        {
+            await _topicProducer.Produce(new VedioDeletedEvents { Title = title });
+            return Ok(title);
+        }
+    }
+}
